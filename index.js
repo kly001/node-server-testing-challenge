b9a8baebb9ce592323e1cae5cee8b84b3cdbfcba
kly001 <karenly1961@gmail.com>
2020-01-04 02:32:08 -0500

    Initialized

---
+++ index.js
@@ -0,0 +1,6 @@
+// require('dotenv').config();
+
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 2345;
+server.listen(port, () => console.log(`\n** Server running on port ${port} **\n`));
